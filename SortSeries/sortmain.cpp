7551f80c64b108f06a2b776562ec6f853809074e
Aosamesan <aosamesan@gmail.com>
2015-01-27 15:00:38 +0900

    edit sortmain.cpp

--- SortSeries/sortmain.cpp
+++ SortSeries/sortmain.cpp
@@ -3,14 +3,32 @@
 using namespace Painter;
 using namespace Sorts;
 
-int main(){
-	cout << "Press Enter Key to Start... ";
-	getchar();
-	
+int main(int argc, char** argv){
 	FramePainter f;
 	PrintSorter ps;
-	unsigned int sizes[] = { 5000, 10000, 30000 };
+	unsigned int numOfTests;
+	unsigned int tmpSize;
+	vector<unsigned int> sizes;
 	
+	if(argc == 1) {
+		try{
+			cin >> numOfTests;
+			for(unsigned int i = 0; i < numOfTests; i++){
+				cin >> tmpSize;
+				if(tmpSize == 0)
+					throw new exception();
+				sizes.push_back(tmpSize);
+			}
+		} catch(exception e){
+			cout << e.what() << endl;
+			return -2;
+		}
+	} else {
+		cerr << "Usage 1 : " << argv[0] << endl;
+		cerr << "Usage 2(Redirection) : " << argv[0] << " < file" << endl;
+		return -1;
+	}
+
 	cout << f.top() << endl;
 	cout << f.header() << endl;
 	for (unsigned int i : sizes){
